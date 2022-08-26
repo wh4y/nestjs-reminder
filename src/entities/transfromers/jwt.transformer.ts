b7db7f1619b3ed2895d6b7c7b98ee60a781eef09
Anton Veselev <wh4y@mail.ru>
2022-08-26 22:14:07 +0300

    token transformer modified

--- src/entities/transfromers/jwt.transformer.ts
+++ src/entities/transfromers/jwt.transformer.ts
@@ -3,12 +3,12 @@ import Id from "../valueObjects/id.value-object";
 import JWT from "../valueObjects/jwt.value-object";
 
 class JwtTransformer implements ValueTransformer {
-  public from(value: string): JWT {
-    return new JWT(value);
+  public from(value: string | null): JWT | null {
+    return value ? new JWT(value) : null;
   }
 
-  public to(token: JWT): string {
-    return token.value;
+  public to(token: JWT | null): string {
+    return token ? token.value : null;
   }
 }
 
