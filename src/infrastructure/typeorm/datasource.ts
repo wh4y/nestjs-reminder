536a24bf631adbbe7b479e5f67bdef71dd921285
Anton Veselev <wh4y@mail.ru>
2022-08-28 14:09:58 +0300

    datasource db host renamed as localhost

--- src/infrastructure/typeorm/datasource.ts
+++ src/infrastructure/typeorm/datasource.ts
@@ -9,14 +9,14 @@ const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
   type: configService.get<string>("DB_DIALECT"),
-  host: configService.get<string>("DB_HOST"),
+  host: "localhost",
   port: configService.get<string>("PORT"),
   username: configService.get<string>("DB_USER"),
   password: configService.get<string>("DB_PASSWORD"),
   database: configService.get<string>("DB_NAME"),
   synchronize: true,
   logging: true,
-  entities: ["./dist/domain/entities/*.entity{.ts,.js}"],
+  entities: ["./dist/domain/entities/**/*.entity{.ts,.js}"],
   migrations: ["./dist/infrastructure/typeorm/migrations/*{.ts,.js}"],
   cli: {
     migrationsDir: "./dist/infrastructure/typeorm/migrations"
