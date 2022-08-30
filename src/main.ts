096efffb272000267ea88121f715e567e192fea7
Anton Veselev <wh4y@mail.ru>
2022-08-30 19:31:05 +0300

    APP_PORT env var added

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("/api/v1");
-  await app.listen(3000);
+
+  const PORT = app.get<ConfigService>(ConfigService).get<number>("APP_PORT");
+
+  await app.listen(PORT);
 }
 
 bootstrap();
